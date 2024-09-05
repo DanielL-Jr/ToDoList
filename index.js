b029dfb3cc2e4eba3efc613e588676f74042da9a
Daniel Lourenço <daniellourenco.contato@gmail.com>
2024-09-05 15:37:50 -0300

    Endpoint GET /tasks criado

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/tasks", async (req, res) => {
+  const { user_id } = req.body;
+  const { data, error } = await lerTarefas(user_id);
+  if (error) {
+    res.status(500).send("Erro ao consultar tabela: ", error);
+  } else {
+    res.status(200).json(data);
+  }
+});
+
 app.listen(8080, () => console.log("Rodando servidor na porta 8080"));
