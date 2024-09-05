3a85a9543b5abff939c1e043ce3a0cd2a335aa66
Daniel Lourenço <daniellourenco.contato@gmail.com>
2024-09-05 15:37:19 -0300

    Servidor Express criado

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require("express");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
 const lerTarefas = require("./database/tasks");
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(8080, () => console.log("Rodando servidor na porta 8080"));
