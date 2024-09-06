3b39707b571d6ae06bb490e9fc2aedf07f40d682
Daniel Lourenço <daniellourenco.contato@gmail.com>
2024-09-06 11:28:48 -0300

    Função de atualizar estado da tarefa adicionada

--- database/tasks.js
+++ database/tasks.js
@@ -3,7 +3,7 @@ const supabase = require("./connection");
 const lerTarefas = async (user_id) => {
   let consulta = await supabase
     .from("tasks")
-    .select("feita, descricao, inicio, fim")
+    .select("id, feita, descricao, inicio, fim")
     .eq("user_id", user_id);
   if (consulta.error) {
     console.log("Erro ao consultar tarefas: ", consulta.error);
@@ -13,4 +13,19 @@ const lerTarefas = async (user_id) => {
   return consulta;
 };
 
-module.exports = lerTarefas;
+const trocarEstado = async (estado, id) => {
+  let { data } = await supabase.from("tasks").select("id").eq("id", id);
+  if (data.length == 0) return { error: "Tarefa não encontrada" };
+  let consulta = await supabase
+    .from("tasks")
+    .update({ feita: estado })
+    .eq("id", id);
+  if (consulta.error) {
+    console.log("Erro ao atualizar a tarefa: ", consulta.error);
+  } else {
+    console.log("Tarefa atualizada com sucesso!");
+  }
+  return consulta;
+};
+
+module.exports = { lerTarefas, trocarEstado };
